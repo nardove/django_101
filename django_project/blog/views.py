f8766389a6ae1026ca47aa310f591626fd06dc4b
Ricardo Sanchez <nardove@yahoo.com>
2021-03-12 06:09:45 +0000

    Part 5 completed

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'Ricardo Sanchez',
-        'title': 'Dummy blog post',
-        'content': 'First post content',
-        'date_posted': 'March 10, 2021'
-    },
-    {
-        'author': 'Thomas Sanchez',
-        'title': 'Second dummy blog post',
-        'content': 'Second post content',
-        'date_posted': 'June 01, 2014'
-    }
-]
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
