45b21e3d95362f4b640e1aab2d162a9e5ecb25c6
Ricardo Sanchez <nardove@yahoo.com>
2021-03-14 20:27:12 +0000

    Change Email settings to use a GMail account

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -131,8 +131,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.mail.yahoo.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS_DJANGO")
+EMAIL_HOST_USER = os.environ.get("GMAIL_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("GMAIL_PASS")
+print(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
