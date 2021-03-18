277d48b96d0b7b1776c2bc2669f46283c11b6778
Ricardo Sanchez <nardove@yahoo.com>
2021-03-18 13:10:01 +0000

    heroku reset

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'e!w*0p*)^k!@rs-l%5@_fxu(+l)b(@qfwh&stctv81+hfhi)+g'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
